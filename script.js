5290631dd59d6feba6ac061bac801670d2accf8b
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-06 23:37:06 +0530

    this code for capture all the element first

---
+++ script.js
@@ -0,0 +1,15 @@
+const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelectorAll('.counter')
+const finalMessage = document.querySelectorAll('.final')
+const replay = document.querySelectorAll('#replay')
+
+runAnimation()
+
+function runAnimation(){
+    nums.forEach((num,idx)=>{
+        console.log(num,idx)
+
+        const nextToLast = nums.length -1
+
+    })
+}
