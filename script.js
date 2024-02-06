aacf88e71fd1337533e745c9385b4cdf3c5394fa
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-06 23:48:58 +0530

    this code for addlistener event add goIn & goOut

--- script.js
+++ script.js
@@ -7,9 +7,22 @@ runAnimation()
 
 function runAnimation(){
     nums.forEach((num,idx)=>{
-        console.log(num,idx)
-
         const nextToLast = nums.length -1
+        
+
+
+        num.addEventListener('animationend', (e) => {
+            if (e.animationName === 'goIn' && idx !== nextToLast) {
+              num.classList.remove('in')
+              num.classList.add('out')
+
+            } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+                num.nextElementSibling.classList.add('in')
+              } else {
+                counter.classList.add('hide')
+                finalMessage.classList.add('show')
+              }
+        })
 
     })
 }
